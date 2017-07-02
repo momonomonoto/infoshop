e7d3309302a9055b6eb5fc89bd8e75a1aad6fcdd
Kirill <908city@gmail.com>
2017-07-02 16:32:33 +0400

    add-prototype-forms-to-storybook

--- stories/index.js
+++ stories/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import DatePicker from 'material-ui/DatePicker';
 
-import Button from './Button';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import DatePicker from 'material-ui/DatePicker';
+import TextField from 'material-ui/TextField';
+import Checkbox from 'material-ui/Checkbox';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+
 import Welcome from './Welcome';
 import Hello from './Hello';
+import Button from './Button';
 
+injectTapEventPlugin();
 
 
 const MuiTheme = (storyFn) => (
@@ -19,9 +25,41 @@ const MuiTheme = (storyFn) => (
 );
 
 
-storiesOf('Hello', module)
+
+storiesOf('Forms', module)
     .addDecorator(MuiTheme)
-    .add('simple Hello', () => <Hello />);
+    .add('DatePicker', () => <DatePicker hintText="Controlled Date Input" autoOk={true}/>)
+    .add('TextField', () => <TextField />)
+    .add('NumbertField', () => <TextField type="number" />)
+    .add('Checkbox', () =>
+        <div>
+            <Checkbox label="One" />
+            <Checkbox label="Two" />
+            <Checkbox label="Three" />
+            <Checkbox label="Four" />
+        </div>)
+    .add('RadioButton', () =>
+        <div>
+            <RadioButtonGroup name="shipName" defaultSelected="community">
+                <RadioButton
+                    value="One"
+                    label="One"
+                />
+                <RadioButton
+                    value="Two"
+                    label="Two"
+                />
+                <RadioButton
+                    value="Three"
+                    label="Three"
+                />
+            </RadioButtonGroup>
+        </div>)
+
+storiesOf('Goods', module)
+    .addDecorator(MuiTheme)
+    .add('AppBar', () => <Hello />);
+
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
