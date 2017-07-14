a478a2d943ff46684faf47bece704ea06adedb5a
Kirill <908city@gmail.com>
2017-07-14 09:50:44 +0400

    add-card-shop

--- stories/index.js
+++ stories/index.js
@@ -14,8 +14,87 @@ import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import Welcome from './Welcome';
 import Hello from './Hello';
 import Button from './Button';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+
+import imageFile from './simple.png';
+import Divider from 'material-ui/Divider';
+
+import Comment from 'material-ui/svg-icons/communication/comment';
+
+
+const image = {
+    src: imageFile,
+    alt: 'my image',
+};
+
 
 injectTapEventPlugin();
+
+
+
+
+const IconMenuExampleSimple = () => (
+    <div>
+        <IconMenu
+            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            style = {{'float':'right'}}
+        >
+            <MenuItem primaryText="Пожаловаться" />
+            <MenuItem primaryText="Отзывы" />
+        </IconMenu>
+    </div>
+);
+const CardExampleWithAvatar = () => (
+    <Card>
+        <IconMenuExampleSimple  />
+        <CardHeader
+            title="Артем Иванушкин"
+            subtitle="НЛП-тренер"
+            avatar={image.src}
+        />
+
+        <CardMedia
+        >
+        </CardMedia>
+        <CardTitle title="НЛП-треннинг" subtitle="Тренируем уверенность в себе" />
+        <CardText>
+
+            Я сам когда-то прошел этот путь. От неуверенного в себе человека до тренера и консультанта, за плечами которого более 400 проведенных тренингов.
+
+            «Тренинг уверенности» — это тренинг самооценки, свободы и ответственности, самостоятельности и — в любом возрасте — зрелости и взросления. Если Вы намерены стать уверенным в себе человеком, с радостью жду Вас на этом тренинге.
+        </CardText>
+        <Divider  />
+        <CardActions>
+            <div style={{'float':'right','paddingTop':'8px'}} >
+                 <Checkbox
+                    checkedIcon={<ActionFavorite />}
+                    style={{display:'inline-block',width:'100px'}}
+                    uncheckedIcon={<ActionFavoriteBorder />}
+                    label="22"
+                />
+                
+                <Checkbox
+                    checkedIcon={<Comment />}
+                    style={{display:'inline-block',width:'60px'}}
+                    uncheckedIcon={<Comment />}
+                    label="3"
+                />
+            </div>
+            <FlatButton  label="Подробнее" />
+        </CardActions>
+    </Card>
+);
 
 
 const MuiTheme = (storyFn) => (
@@ -56,10 +135,13 @@ storiesOf('Forms', module)
             </RadioButtonGroup>
         </div>)
 
+
+
+
 storiesOf('Goods', module)
     .addDecorator(MuiTheme)
-    .add('AppBar', () => <Hello />);
-
+    .add('AppBar', () => <Hello />)
+    .add('SimpleCard',()=><div><CardExampleWithAvatar/></div>);
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
